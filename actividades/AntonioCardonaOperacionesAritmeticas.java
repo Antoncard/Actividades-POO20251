41ee6587aa0372147de82fe50995fd4eb8eeaa1e
Antoncard <antonio.cardona60111@ucaldas.edu.co>
2025-04-01 09:14:59 -0500

    Add files via upload

---
+++ actividades/AntonioCardonaOperacionesAritmeticas.java
@@ -0,0 +1,26 @@
+
+public class AntonioCardonaOperacionesAritmeticas {
+    public static void main(String[] args) {
+
+        //DECLARAR VARIABLES
+        int numA;
+        int numB;
+
+        //ASIGANCION DE VARAIBLES
+        numA = 89;
+        numB = 323;
+
+        //VARIABLES EN CONSOLA
+        System.out.println("El numero A es " + numA);
+        System.out.println("El numero B es " + numB);
+
+        //operaciones en la consola
+        System.out.println("La suma de numero A y numero B es: "+(numA+numB));
+        System.out.println("la multiplicacion de numero A y numero B es: "+(numA*numB));
+        System.out.println("la resta de numero A y numero B es: "+(numA-numB));
+        System.out.println("la division de numero A y numero B es: "+(numB/numA));
+
+
+
+    }
+}
